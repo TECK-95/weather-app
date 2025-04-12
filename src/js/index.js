f673861ba33e9ebd47c29f4ba41a1173df86fbd7
Thomas Eckersley <ecke2225@mylaurier.ca>
2025-04-12 16:36:48 -0400

    Set up files, folders and webpack to begin project

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import "../styles/styles.css";
+import odinImage from "../images/odin.png";
+
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
+
+console.log("Weather TEck");
